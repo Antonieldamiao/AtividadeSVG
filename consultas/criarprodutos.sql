bbec4af13fada7e89c4fc36a6a9331946cc58077
Antoniel Damiao <nielpoow@gmail.com>
2020-02-20 01:52:59 -0300

    adicionado os scripts do banco

--- consultas/criarprodutos.sql
+++ consultas/criarprodutos.sql
@@ -5,4 +5,11 @@ create table produto (
 	primary key(id)
 );
 
-insert into produto(nome,preco) values ('litro de pitu' , '2.50');
+insert into produto(nome,preco) values ('Arroz' , '4.50');
+insert into produto(nome,preco) values ('feijjão com gugui', '12.50');
+insert into produto(nome,preco) values ('pao durmido', '2.50');
+insert into produto(nome,preco) values ('corote' , '2.50');
+insert into produto(nome,preco) values ('litro de pitu' , '5.50');
+insert into produto(nome,preco) values ('litro de turarina' , '7.50');
+insert into produto(nome,preco) values ('limão tiragosto' , '2.50');
+insert into produto(nome,preco) values ('sapupara' , '2.50');
